015eaefc97eca127433c7b697f75e52481e265fc
Falk Hoppe <falkh@Aenima.local>
2013-03-12 08:45:21 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Associates::Application.config.secret_token = '91e9d52843b63e53a4dfee90bb80084439f9216c5454dc2bd85a92eae65c3602a6660935164eeedc44084135051d2f9d875a19c2be149d1352df1980d2b92f51'
